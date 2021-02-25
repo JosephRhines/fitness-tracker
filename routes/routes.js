30cbff475a6818b9da8853d39559268a87b26409
Joseph Rhines <jprhines@gmail.com>
2021-02-24 20:09:07 -0600

    I added slashes I was missing in my routes in front of api, changed find to findByIdAndUpdate in the put route, added an aggregate function to the get route and added a sort function to the get range route

--- routes/routes.js
+++ routes/routes.js
@@ -1,3 +1,4 @@
+const  db = require("../models");
 var path = require("path");
 
 module.exports = function (app) {
@@ -13,6 +14,61 @@ app.get("/exercise", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
+app.get("/api/workouts", function(req, res){
+    db.Workout.aggregate([{
+        $addFields:{
+            totalDuration:{
+                $sum: "$exercises.duration"
+            }
+        }
+    }])
+    .then(function (data){
+        res.json(data);
+    })
+    .catch(function(err){
+        res.json(err);
+    });
+});
 
+app.put("/api/workouts/:id", function(req, res){
+    db.Workout.findByIdAndUpdate(req.params.id, {$push: {exercises: req.body}}, {new:true})
+    .then(function(data){
+        res.json(data);
+    })
+    .catch(function(err){
+        res.json(err);
+    });
+});
+
+
+app.post("/api/workouts", function (req, res){
+    db.Workout.create(req.body)
+    .then(function(data){
+        res.json(data);
+    })
+    .catch(function(err){
+        res.json(err);
+    });
+});
+
+app.get("/api/workouts/range", function (req, res){
+    db.Workout.aggregate([{
+        $addFields:{
+            totalDuration:{
+                $sum: "$exercises.duration"
+            }
+        }
+    }])
+    .sort({
+        _id:-1
+    })
+    .limit(7)
+    .then(function(data){
+        res.json(data);
+    })
+    .catch(function(err){
+        res.json(err);
+    });
+});
 
 }
